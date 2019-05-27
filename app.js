8843acb10f22aade681d0022d21995e3f61a1c34
Rene55 <ranarene179@gmail.com>
2019-05-27 16:03:05 -0500

    subiendo mi foto

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/informacion', informacionRouter);
+app.use('/informacion/', informacionRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
