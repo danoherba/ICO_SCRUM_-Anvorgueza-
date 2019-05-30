eba8c3db8745b1016ec5171ab5643abb500784a7
angelzurita <zurita.ska8@gmail.com>
2019-05-30 13:44:39 -0500

    Se modifica pagina SCRUM y se agregan apartados

--- routes/apartado3.js
+++ routes/apartado3.js
@@ -6,8 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('scrum', { title: 'Express' });
 });
 
-router.get('/renep', function(req, res, next) {
-  res.render('rene', { title: 'Express' });
+router.get('/herramienta', function(req, res, next) {
+  res.render('herramientas', { title: 'Express' });
+});
+
+router.get('/metodologias', function(req, res, next) {
+  res.render('metodologia', { title: 'Express' });
+});
+
+router.get('/role', function(req, res, next) {
+  res.render('roles', { title: 'Express' });
+});
+
+router.get('/scrumm', function(req, res, next) {
+  res.render('scru', { title: 'Express' });
 });
 
 
